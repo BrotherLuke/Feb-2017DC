beda016c78b946744ab5e5c5ff3dabb08173ab92
BrotherLuke <game110pm@605-110-STU02>
2017-02-01 13:06:19 -0700

    if else statements

---
+++ 2 1 17/2 1 17/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char input;
+	cout << "Do you like the seahawks? (y or n)" << endl;
+	cin >> input;
+	if (input == 'y')
+		cout << "Wow you're super funky cool" << endl;
+	else if (input ==  'n')
+		cout << "You dirty piece of trash" << endl;
+	
+	
+
+
+
+}
